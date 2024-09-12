dd9d3c1e75b26b0cb1b18e8f34688e6e07c04774
Daniel Granados <dandiegogranados@yahoo.com>
2024-09-12 15:00:05 -0600

    Read campaigns

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -1,28 +1,35 @@
+"use client";
 import Image from "next/image";
-import Button from "react-bootstrap/Button"
+import Button from "react-bootstrap/Button";
 import Link from "next/link";
-
+import CampaignCard from "@/components/CampaignCard";
+import { db } from "@/lib/firebase/config";
+import { ref, onValue } from "firebase/database";
+import { useState } from "react";
 export default function Home() {
+  const campaignsRef = ref(db, "campaigns");
+  const [campaigns, setCampaigns] = useState({});
+
+  onValue(
+    campaignsRef,
+    (snapshot) => {
+      console.log(snapshot.val());
+      setCampaigns(snapshot.val());
+    },
+    { onlyOnce: true }
+  );
+
   return (
     <main className="container">
-      <div className="row col-lg-6 px-5 my-5"  style={{ boxShadow: '0 4px 6px rgba(0, 0, 0, 0.3)' }}>
-        <div className="col">       
-          <img src='' alt='imagen' />
-        </div>
-        <div className="col my-5">  
-          <div className="campaign-info">
-            <h2>Título</h2>
-            <div className="">
-              <p><strong>Fecha</strong> </p>
-              <p><strong>Lugar</strong> </p>
-              <p><strong>Cupos</strong></p>
-            </div>
-            <Button variant="primary" type="submit">
-            Ver más
-            </Button>
-          </div>
-          </div>
-        </div>
+      <h1>Asociación Animalitos Abandonados</h1>
+      {Object.keys(campaigns)
+        .reverse()
+        .map((campaign) => {
+          return (
+            <CampaignCard campaign={{ campaign, ...campaigns[campaign] }} />
+          );
+        })}
+      {/* <CampaignCard />  */}
       <Link href="adminlogin">Ingresar como administrador</Link>
     </main>
   );
---
+++ components/CampaignCard.js
@@ -0,0 +1,44 @@
+import { Button } from "react-bootstrap";
+import Carousel from "react-bootstrap/Carousel";
+import Link from "next/link";
+export default function CampaignCard({ campaign }) {
+  return (
+    <article
+      className="container row col-lg-6 px-5 my-5"
+      style={{ boxShadow: "0 4px 6px rgba(0, 0, 0, 0.3)" }}
+    >
+      <Carousel>
+        {campaign.photos.map((photo) => (
+          <Carousel.Item key={photo}>
+            <img className="d-block w-100" src={photo} alt="" />
+          </Carousel.Item>
+        ))}
+      </Carousel>
+      <div className="col my-5">
+        <div className="campaign-info">
+          <h2>{campaign.title}</h2>
+          <div className="">
+            <p>
+              <strong>Fecha: {campaign.date}</strong>{" "}
+            </p>
+            <p>
+              <strong>Lugar: {campaign.place}</strong>{" "}
+            </p>
+            <p>
+              <strong>Cupos: {0}</strong>
+            </p>
+          </div>
+          <Link href="/campaign">
+            <Button
+              variant="primary"
+              type="submit"
+              aria-label={`Ver información de ${campaign.title} en ${campaign.place} el día ${campaign.date}`}
+            >
+              Ver más
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+}
