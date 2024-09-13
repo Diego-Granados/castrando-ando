c8963d0f6b2ba6e59ac69be10cb2ec43bd8c7f20
Brayton16 <Brayton2011@hotmail.es>
2024-09-12 21:03:29 -0600

    Estilos de varias paginas

--- app/(main)/contacto/page.js
+++ app/(main)/contacto/page.js
@@ -1,9 +1,40 @@
 import Image from "next/image";
+import { Form } from "react-bootstrap";
+import ContactForm from "./ContactoForm";
 
-export default function Contacto() {
+
+function Contacto() {
+  async function contacto(params) {
+    "use server";
+
+    //Se debe mandar los correos
+  }
+
   return (
     <main className="container">
-      <h1>Asociación Animalitos Abandonados</h1>
+      <h1>Contáctenos</h1>
+      <div className="row">
+        <div className="col" id="form">
+          <ContactForm contacto={contacto}></ContactForm>
+        </div>
+        <div className="col" id="img&sinpe">
+            <img src="https://www.pdsa.org.uk/media/7646/golden-retriever-gallery-2.jpg?anchor=center&mode=crop&quality=100&height=500&bgcolor=fff&rnd=133020229510000000"/>
+            <p className="mt-3">
+              Si desea realizar donaciones o llamarnos, puede realizar un Sinpe a
+              los siguientes números de teléfono:
+            </p>
+            <ul>
+              <li>8594-7283: Lorena Jiménez</li>
+              <li>8585-8505: Lorena Jiménez</li>
+            </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default Contacto;
+{/* <h1 className="">Asociación Animalitos Abandonados</h1>
       <h2>Contáctanos</h2>
       <div>
         <p>
@@ -14,7 +45,4 @@ export default function Contacto() {
           <li>8594-7283: Lorena Jiménez</li>
           <li>8585-8505: Lorena Jiménez</li>
         </ul>
-      </div>
-    </main>
-  );
-}
+      </div> */}
