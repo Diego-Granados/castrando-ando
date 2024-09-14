bbf93d308f1916cb45775f517f63493d66fd5ddf
Hytan Jara <hytanjara69@gmail.com>
2024-09-14 15:28:37 -0600

    About us

--- app/(main)/aboutus/page.js
+++ app/(main)/aboutus/page.js
@@ -3,9 +3,30 @@ import Image from "next/image";
 export default function AboutUs() {
   return (
     <main className="container">
-      <h1>Asociación Animalitos Abandonados</h1>
-      <h2>¿Quiénes somos?</h2>
-      <h2>Indicaciones para las citas</h2>
+      <h1 className="text-center" style={{color:'#2055A5'}}>Asociación Animalitos Abandonados</h1>
+      <div className="container row mt-5">
+        <h2 className="text-center">¿Quiénes somos?</h2>
+        <article className="col">
+          <p>
+            <font size='6'>Somos una asociación que busca el bienestar de animalitos que se 
+              encuentren en situación de calle y necesiten nuestra ayuda para poder vivir una vida en perfectas condiciones
+            </font>
+          </p>
+        </article>
+        <img className="img-fluid col" style={{maxHeight:'50vh', width:'auto'}} src="https://scontent.fsjo6-1.fna.fbcdn.net/v/t1.6435-9/53892547_2233470650051358_366009317599477760_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=13d280&_nc_ohc=fUAsc4KTWV4Q7kNvgH6-6gM&_nc_ht=scontent.fsjo6-1.fna&_nc_gid=AuGJ4f1bv81ygkNFKr18Jwn&oh=00_AYDhQtMgjv2BbCuRRwfPSZRmF17RBAe9oDG_ofHCxN4aEg&oe=670D4ACC"></img>
+      </div>
+      <div className="container row mt-5">
+        <h2 className="mb-4 text-center">Indicaciones para las citas</h2>
+        <img className="img-fluid col" style={{maxHeight:'50vh', width:'auto'}} src="https://i.pinimg.com/564x/3b/68/d0/3b68d096d40b378ed567b506312b17c6.jpg"></img>
+        <article className="col">
+          <h2>¿Quiénes somos?</h2>
+          <p>
+            <font size='6'>Somos una asociación que busca el bienestar de animalitos que se 
+              encuentren en situación de calle y necesiten nuestra ayuda para poder vivir una vida en perfectas condiciones
+            </font>
+          </p>
+        </article>
+      </div>
     </main>
   );
 }
--- app/(main)/adminlogin/page.js
+++ app/(main)/adminlogin/page.js
@@ -6,21 +6,9 @@ import { toast } from "react-toastify";
 function Login() {
   return (
     <main className="">
-      <div className="row px-0 py-0" style={{ maxHeight: "100vh" }}>
-        <div
-          className="col-md-6 d-flex justify-content-center align-items-center px-0"
-          style={{ backgroundColor: "blue" }}
-        >
-          <img
-            class="img-fluid my-5 rounded max-height-20"
-            src="https://i.pinimg.com/736x/c6/ab/8a/c6ab8a727ffc6784e59246e60f22b018.jpg"
-          ></img>
-        </div>
-        <div className="col-md-5 p-5 rounded-start-3 bg-F4F6F0">
-          <div className="row">
-            <h1 className="display-6 my-4 text-lg-start">Inicia sesión</h1>
-            <p>¡Bienvenido!</p>
-            <p>Por favor, ingrese sus datos de acceso</p>
+      <div className="row px-0 py-0" style={{maxHeight:'100vh'}}>
+          <div className="col-md-6 d-flex justify-content-center align-items-center px-0" style={{backgroundColor: 'blue'}}>
+              <img class="img-fluid my-5 rounded max-height-20" src="https://i.pinimg.com/736x/c6/ab/8a/c6ab8a727ffc6784e59246e60f22b018.jpg"></img>
           </div>
           <div className="row flex items-center justify-center">
             <LoginForm />
