22158956e894dafc183dbe8f0cef465d1b4991b5
Daniel Granados <dandiegogranados@yahoo.com>
2025-01-28 15:02:02 -0600

    Campaign tests

--- __tests__/main/main.test.js
+++ __tests__/main/main.test.js
@@ -1,13 +1,13 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import Home from "@/app/(main)/page";
+// import "@testing-library/jest-dom";
+// import { render, screen } from "@testing-library/react";
+// import Home from "@/app/(main)/page";
 
-describe("Home", () => {
-  it("renders a heading", () => {
-    render(<Home />);
+// describe("Home", () => {
+//   it("renders a heading", () => {
+//     render(<Home />);
 
-    const heading = screen.getByRole("heading", { level: 1 });
+//     const heading = screen.getByRole("heading", { level: 1 });
 
-    expect(heading).toBeInTheDocument();
-  });
-});
+//     expect(heading).toBeInTheDocument();
+//   });
+// });
